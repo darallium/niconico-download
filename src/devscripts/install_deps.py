9eabc96eb57525f3d06c709c1317f3d0b6fea6d5
darallium <contact@darallium.com>
2025-01-18 17:23:55 +0900

    s->os

--- src/devscripts/install_deps.py
+++ src/devscripts/install_deps.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-sys.path.insert(0, s.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import argparse
 import re
